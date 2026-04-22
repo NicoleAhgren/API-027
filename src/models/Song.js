c9a042418065a9a938624965781f62ebae081372
Nicole Åhgren <na223jy@student.lnu.se>
2026-04-22 13:39:16 +0200

    add Song model with schema definition

---
+++ src/models/Song.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const songSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  daysReleased: Number,
+  position: Number,
+  top10: Number,
+  peakPosition: Number,
+  peakPositionTimes: String,
+  peakStreams: Number,
+  totalStreams: Number
+})
+
+export default mongoose.model('Song', songSchema)
