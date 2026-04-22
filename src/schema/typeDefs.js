424efad3244a3c8345f0d4048c1b0844c4687dc5
Nicole Åhgren <na223jy@student.lnu.se>
2026-04-22 13:38:44 +0200

    add GraphQL type definitions for Song, Artist, ChartEntry, User, AuthPayload, Query, and Mutation

---
+++ src/schema/typeDefs.js
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag'
+
+const typeDefs = gql`
+  type Song {
+    id: ID!
+    title: String!
+    artist: String!
+    daysReleased: Int
+    position: Int
+    top10: Int
+    peakPosition: Int
+    peakPositionTimes: String
+    peakStreams: Float
+    totalStreams: Float
+  }
+  
+  type Artist {
+    name: String!
+    songs: [Song!]!
+    numberOfSongs: Int!
+    totalStreams: Float!
+    }
+
+    type ChartEntry {
+    position: Int!
+    song: Song!
+    }
+
+    type User {
+    id: ID!
+    username: String!
+    }
+
+    type AuthPayload {
+    token: String!
+    user: User!
+    }
+
+    type Query {
+    songs(page: Int, limit: Int, search: String): [Song!]!
+    song(id: ID!): Song
+    artists(page: Int, limit: Int): [Artist!]!
+    artist(name: String!): Artist
+    topChart(limit: Int): [ChartEntry!]!
+    }
+
+    type Mutation {
+    register(username: String!, password: String!): AuthPayload!
+    login(username: String!, password: String!): AuthPayload!
+    addSong(title: String!, artist: String!): Song!
+    updateSong(id: ID!, title: String, artist: String): Song!
+    deleteSong(id: ID!): Boolean!
+    }
+  `
