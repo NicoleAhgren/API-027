2de574f38ea7e5041e8b9cb6b194fcd7d434cc1f
Nicole Åhgren <na223jy@student.lnu.se>
2026-05-11 14:13:16 +0200

    done with the query part in resolvers in index.js

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,13 +1,68 @@
-import jwt from 'jsonwebtoken'
-import Song from '../models/Song.js'
-import User from '../models/User.js'
-import { requireAuth } from '../middleware/auth.js'
+import jwt from "jsonwebtoken"
+import Song from "../models/Song.js"
+import User from "../models/User.js"
+import { requireAuth } from "../middleware/auth.js"
 
 const resolvers = {
   Query: {
-    songs: async (__, { page = 1, limit = 20, search}) => {
-      const query = search ? { $or: [{title: new RegExp(search, 'i')}, {artist: new RegExp(search, 'i')}] } : {}
-      return Song.find(query).skip((page - 1) * limit).limit(limit)
-    }
-  }
+    songs: async (__, { page = 1, limit = 20, search }) => {
+      const query = search
+        ? {
+            $or: [
+              { title: new RegExp(search, "i") },
+              { artist: new RegExp(search, "i") },
+            ],
+          }
+        : {}
+      return Song.find(query)
+        .skip((page - 1) * limit)
+        .limit(limit)
+    },
+
+    song: async (__, { id }) => {
+      const song = await Song.findById(id)
+      if (!song) throw new Error("Song not found")
+      return song
+    },
+
+    artists: async (__, { page = 1, limit = 20 }) => {
+      const result = await Song.aggregate([
+        {$group: {
+            _id: "$artist",
+            songs: { $push: "$$ROOT" },
+            numberOfSongs: { $sum: 1 },
+            totalStreams: { $sum: "$totalStreams" },
+          },
+        },
+        { $sort: { totalStreams: -1 } },
+        { $skip: (page - 1) * limit },
+        { $limit: limit },
+      ])
+      return result.map((a) => ({
+        name: a._id,
+        songs: a.songs,
+        numberOfSongs: a.numberOfSongs,
+        totalStreams: a.totalStreams,
+      }))
+    },
+    artist: async (__, { name }) => {
+      const result = await Song.aggregate([
+        { $match: { artist: name } },
+        { $group: {
+            _id: "$artist",
+            songs: { $push: "$$ROOT" },
+            numberOfSongs: { $sum: 1 },
+            totalStreams: { $sum: "$totalStreams" },
+         } }
+      ])
+      if (!result.length) throw new Error('Artist not found')
+        const artistData = result[0]
+        return { name: artistData._id, songs: artistData.songs, numberOfSongs: artistData.numberOfSongs, totalStreams: artistData.totalStreams}
+      },
+
+      topChart: async (__, { page = 1, limit = 20 }) => {
+        const songs = await Song.find({ position: { $exists: true } }).sort({ position: 1 }).limit(limit)
+        return songs.map(s => ({ position: s.position, song: s}))
+      },
+  },
 }
