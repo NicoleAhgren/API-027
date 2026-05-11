b364ea3b757ad1af9a5ac39531edb968f185c29c
Nicole Åhgren <na223jy@student.lnu.se>
2026-05-11 16:03:09 +0200

    feat: add Apollo Server plugin for enhanced landing page experience

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
 import mongoose from 'mongoose'
 import typeDefs from './schema/typeDefs.js'
 import { getUser } from './middleware/auth.js'
@@ -11,7 +12,12 @@ async function start() {
   await mongoose.connect(process.env.MONGODB_URI)
   console.log('Connected to MongoDB')
 
-  const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+  })
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: process.env.PORT || 2000 },
